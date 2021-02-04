137f5bf40c2c5615d8916ac5204f1dc90df82c67
Faraz Mundia <farazmuhammad1995@gmail.com>
2021-02-04 15:11:56 +0500

    fix style issue and render properly with react snap

--- src/index.js
+++ src/index.js
@@ -6,21 +6,17 @@ import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import * as serviceWorker from "./serviceWorker";
+import { hydrate, render } from "react-dom";
 
-/* const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root");
+const firstChildCss = rootElement.children[0]
+firstChildCss.style = "";
+
 if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
+  hydrate( <BrowserRouter> <App /> </BrowserRouter> , rootElement);
 } else {
-  render(<App />, rootElement);
-} */
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
-
+  render(<BrowserRouter> <App /> </BrowserRouter> , rootElement);
+} 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
